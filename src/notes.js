f4f82ba35fb05e7463cd5ddf1955e811d7cc8c88
Nitesh Patel <72400641+NiteshPatel4@users.noreply.github.com>
2022-07-15 15:06:58 +0530

    Add files via upload

---
+++ src/notes.js
@@ -0,0 +1,24 @@
+const notes = [
+  {
+    key: 1,
+    title: "Delegation",
+    content: "Q. How many programmers does it take to change a light bulb?"
+  },
+  {
+    key: 2,
+    title: "Loops",
+    content: "How to keep a programmer in a shower forever"
+  },
+  {
+    key: 3,
+    title: "hello",
+    content: "Hello guys welcome to my yt guys"
+  },
+  {
+    key: 4,
+    title: "Software vs Hardware",
+    content: "Soft vs hard "
+  }
+];
+
+export default notes;
